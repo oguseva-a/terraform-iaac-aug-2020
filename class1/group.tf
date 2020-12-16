fa9240aecb26a2c31c2a18b3bff44c28a0e714ad
Olga Guseva <olchaguseva@gmail.com>
2020-12-16 02:07:41 +0000

    added policy and role

--- class1/group.tf
+++ class1/group.tf
@@ -3,11 +3,29 @@ resource "aws_iam_group" "developers" {
   path = "/"
 }
 
+resource "aws_iam_group" "management" {
+  name = "management"
+  path = "/"
+}
+
+
 resource "aws_iam_group_membership" "developers_team" {
   name = "developers_group_membership"
   users = [
-      "${aws_iam_user.tim.name}"
+      "${aws_iam_user.tim.name}",
+      "${aws_iam_user.bob.name}",
+      "${aws_iam_user.ben.name}"
   ]
   group = "${aws_iam_group.developers.name}"
  
+ 
+}
+
+resource "aws_iam_group_membership" "management_team" {
+  name = "management_group_membership"
+  users = [
+      "${aws_iam_user.ben.name}"
+  ]
+  group = "${aws_iam_group.management.name}"
+
 }
--- class1/user.tf
+++ class1/user.tf
@@ -2,3 +2,11 @@ resource "aws_iam_user" "tim" {
   name = "tim"
   path = "/"
 }
+resource "aws_iam_user" "bob" {
+  name = "bob"
+  path = "/"
+}
+resource "aws_iam_user" "ben" {
+  name = "ben"
+  path = "/"
+}
