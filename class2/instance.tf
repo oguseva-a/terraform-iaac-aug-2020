68bc328bc7f82bc71b813aa5cc164f9a2d9b1cd6
Olga Guseva <olchaguseva@gmail.com>
2020-12-17 04:00:30 +0000

    added key

--- class2/instance.tf
+++ class2/instance.tf
@@ -1,4 +1,7 @@
 resource "aws_instance" "web" {
     ami = "ami-04bf6dcdc9ab498ca" 
     instance_type = "t2.micro"
+    associate_public_ip_address =  "true"
+    key_name   = "${aws_key_pair.class.key_name}"
 }
+
